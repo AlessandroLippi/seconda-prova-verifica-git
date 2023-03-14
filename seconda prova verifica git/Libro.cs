cc016f59413617c445a016e98ce6c4e18cfc58a0
AlessandroLippi <alessandro.lippi.stud@ispascalcomandini.it>
2023-03-14 16:22:53 +0100

    implementazione della classe Libro

--- seconda prova verifica git/Libro.cs
+++ seconda prova verifica git/Libro.cs
@@ -8,5 +8,40 @@ namespace seconda_prova_verifica_git
 {
     internal class Libro
     {
+
+        public class Libro
+        {
+            public string autore { get; set; }
+            public string editore { get; set; }
+            public string titolo { get; set; }
+            public string pubblicazione { get; set; }
+            public string nPagine { get; set; }
+            public string prestito { get; set; }
+
+
+            public Libro(string a, string e, string t, string p, string nP, string pr)
+            {
+                this.autore = a;
+                this.editore = e;
+                this.titolo = t;
+                this.pubblicazione = p;
+                this.nPagine = nP;
+                this.prestito = pr;
+            }
+
+
+            // metodi:
+
+            public string ToStringLibro()
+            {
+                return "autore: " + autore + "editore: " + editore + "titolo: " + titolo + "pubblicazione: " + pubblicazione + "numero di pagine: " + nPagine + "è in prestito: " + prestito;
+
+            }
+
+            public void TimeToRead()
+            {
+                //if (Libro.nPagine) // come creare un oggetto generico che mi permetta di fare questo controllo su qualsiasi oggetto che chiama questo metodo
+            }
+        }
     }
 }
